ae55b86f741832064e5e21b243830d9e3af36903
makeshift-dinosaur <claire.ezzelle@outlook.com>
2026-06-06 13:13:33 -0400

    hi

---
+++ src/main/java/net/jesterslover/lostmarble/LostMarbleAdditions.java
@@ -0,0 +1,16 @@
+package net.jesterslover.lostmarble;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LostMarbleAdditions implements ModInitializer {
+	public static final String MOD_ID = "lostmarbleadditions";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
